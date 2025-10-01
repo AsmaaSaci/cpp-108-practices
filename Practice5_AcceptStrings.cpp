85c92688692786dc36d40994ac5955ac564fc981
Asmaa Saci <imyasmaa3@gmail.com>
2025-10-01 14:12:19 +0100

    Create Practice5_AcceptStrings.cpp

---
+++ Practice5_AcceptStrings.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std ;
+
+ int main()
+ {
+char strl [200];
+cout << "String Input \n" << endl;
+
+cout << "Insert some a strings: " << endl;
+
+cin.getline (strl ,200);
+ 
+cout << "You entered: " << strl  << endl;
+
+return 0;
+}
