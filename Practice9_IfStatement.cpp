335465623360e5132f52bc5db408002b6d9e5fe3
Asmaa Saci <imyasmaa3@gmail.com>
2025-10-02 18:57:13 +0100

    Create Practice9_IfStatement.cpp

---
+++ Practice9_IfStatement.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 40;
+    if (x > 35) {
+        cout << "the values of x is above expected result" << endl;
+    }
+    cout << "you inserted " << x << endl;
+    return 0;
+}
