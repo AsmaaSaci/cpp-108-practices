790df0fec5ffdbf37f1a3302f1c8374db9350e99
Asmaa Saci <imyasmaa3@gmail.com>
2025-10-03 23:41:08 +0100

    Create Practice25_Percentage.cpp

---
+++ Practice25_Percentage.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float total, obtained;
+    cin >> total >> obtained;
+    float percent = (obtained/total)*100;
+    cout << percent;
+}
