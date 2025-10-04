ac3449b9ce3c480d016483ce3ab466050ff2cd52
Asmaa Saci <imyasmaa3@gmail.com>
2025-10-04 08:59:23 +0100

    Create Practice44_MathFunctions.cpp

---
+++ Practice44_MathFunctions.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    double x;
+    cin >> x;
+    cout << sqrt(x) << " " << cbrt(x) << " " << pow(x,2) << " " << abs(-x);
+}
